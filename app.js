6727580057f711f228ec96d2135e57b0938fdd2e
akazitoo <akds@cesar.school>
2025-05-21 21:17:59 -0300

    changing port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Dados de conexão com o RDS
 const db = mysql.createConnection({
